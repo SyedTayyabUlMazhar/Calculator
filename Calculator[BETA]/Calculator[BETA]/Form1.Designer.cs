94c33f7144ab241a4c62ec8a7a2caacab5e636f2
SyedTayyabUlMazhar <43899564+SyedTayyabUlMazhar@users.noreply.github.com>
2018-10-06 09:43:20 +0500

    Add files via upload

---
+++ Calculator[BETA]/Calculator[BETA]/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Calculator_BETA_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+
+        #endregion
+
+        private System.Windows.Forms.TextBox display;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button DEL;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button add;
+
+
+        //1- Allocates memory for buttons,texboxes... using new just like     obj = new MyClass();
+        //2- Sets properties of those objects(like size and position of button).
+        private void InitializeComponent()
+        {
+            this.display = new System.Windows.Forms.TextBox();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.DEL = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Enabled = false;
+            this.display.Location = new System.Drawing.Point(12, 12);
+            this.display.Multiline = true;
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(246, 50);
+            this.display.TabIndex = 0;
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.SystemColors.Control;
+            this.seven.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.seven.FlatAppearance.BorderSize = 0;
+            this.seven.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seven.ForeColor = System.Drawing.Color.Black;
+            this.seven.Location = new System.Drawing.Point(12, 68);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(61, 44);
+            this.seven.TabIndex = 1;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(79, 68);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(61, 44);
+            this.eight.TabIndex = 2;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(146, 68);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(61, 44);
+            this.nine.TabIndex = 3;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(12, 118);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(61, 44);
+            this.four.TabIndex = 4;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(79, 118);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(61, 44);
+            this.five.TabIndex = 5;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(146, 118);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(61, 44);
+            this.six.TabIndex = 6;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(12, 168);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(61, 44);
+            this.one.TabIndex = 9;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(79, 168);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(61, 44);
+            this.two.TabIndex = 8;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(146, 168);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(61, 44);
+            this.three.TabIndex = 7;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // point
+            // 
+            this.point.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.point.Location = new System.Drawing.Point(12, 218);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(61, 44);
+            this.point.TabIndex = 10;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(146, 218);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(61, 44);
+            this.equal.TabIndex = 11;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(79, 218);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(61, 44);
+            this.zero.TabIndex = 12;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // DEL
+            // 
+            this.DEL.Location = new System.Drawing.Point(213, 68);
+            this.DEL.Name = "DEL";
+            this.DEL.Size = new System.Drawing.Size(45, 37);
+            this.DEL.TabIndex = 13;
+            this.DEL.Text = "DEL";
+            this.DEL.UseVisualStyleBackColor = true;
+            this.DEL.Click += new System.EventHandler(this.DEL_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(213, 159);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(45, 35);
+            this.multiply.TabIndex = 14;
+            this.multiply.Text = "x";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(213, 111);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(45, 42);
+            this.divide.TabIndex = 15;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Location = new System.Drawing.Point(213, 200);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(45, 28);
+            this.subtract.TabIndex = 16;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(213, 234);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(45, 28);
+            this.add.TabIndex = 17;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(268, 274);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.DEL);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+    }
+}
+
---
+++ Calculator[BETA]/Calculator[BETA]/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_BETA_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            display.Text += zero.Text;
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            display.Text += one.Text;
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            display.Text += two.Text;
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            display.Text += three.Text;
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            display.Text += four.Text;
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            display.Text += five.Text;
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            display.Text += six.Text;
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            display.Text += seven.Text;
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            display.Text += eight.Text;
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            display.Text += nine.Text;
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            display.Text += point.Text;
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            display.Text += divide.Text;
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            display.Text += multiply.Text;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            display.Text += add.Text;
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            display.Text += subtract.Text;
+        }
+
+        /*
+        private void equal_Click(object sender, EventArgs e)
+        {
+            
+                
+                string exp = display.Text; // 28.25
+
+                string expCopy = exp;
+
+                if (expCopy[0] != '+' && expCopy[0] != '-') expCopy = expCopy.Insert(0, "+");
+
+                char Operator; // the operator that we will evaluate ('x')
+                int index;     // the index of the operator that we will evaluate
+
+                char[] operators = { 'x', '/', '+', '-' };
+                for (int j = 0, k = 1; k < 4; j +=2, k+=2)
+                {
+                    bool hasOperatorJ = (expCopy.IndexOf(operators[j], 1) != -1);
+                    bool hasOperatorK = (expCopy.IndexOf(operators[k], 1) != -1);
+
+                    int indexOfOperatorJ = expCopy.IndexOf(operators[j], 1);
+                    int indexOfOperatorK = expCopy.IndexOf(operators[k], 1);
+
+                    while (hasOperatorJ || hasOperatorK) //while atleast one of 'x' and '/' or '+' and '-' is present when searching from [1] 
+                    {
+
+                        // If both 'x' and '/' when 'j'=0 are present, OR if both '+' and '-' when j=2 are present 
+                        if (hasOperatorJ && hasOperatorK)
+                        {
+                            //then the index = the index of operator which precedes the other
+                            index = (indexOfOperatorJ < indexOfOperatorK) ? indexOfOperatorJ : indexOfOperatorK;
+                        }
+                        else // if one of them is present
+                        {
+                            //then the index = the index of the operator which is present
+                            index = (hasOperatorJ) ? indexOfOperatorJ : indexOfOperatorK;
+                        }
+
+                        Operator = expCopy[index];
+
+                        string op1 = "", op2 = ""; // operands of 'Operator'
+                        string resultOfOperation = ""; // the result of expression ---> op1 Operator op2
+
+                        int i;
+
+                        //get 'op1'
+                        for (i = index - 1; expCopy[i] != '+' && expCopy[i] != '-'; i--)
+                        {
+                            op1 = op1.Insert(0, expCopy[i].ToString());
+                        }
+                        op1 = op1.Insert(0, expCopy[i].ToString());
+
+                        //get 'op2'
+                        for (i = index + 1; (expCopy[i] != '+' && expCopy[i] != '-' && expCopy[i] != operators[j] && expCopy[i] != operators[j + 1]); i++)
+                        {
+                            op2 += expCopy[i];
+                            if (i == expCopy.Length - 1) break;
+                        }
+
+                        if (Operator == 'x' || Operator == '/')
+                            resultOfOperation = (Operator == operators[j]) ?
+                                                (Convert.ToDouble(op1) * Convert.ToDouble(op2)).ToString()
+                                                :
+                                                (Convert.ToDouble(op1) / Convert.ToDouble(op2)).ToString();
+                        else
+                            resultOfOperation = (Operator == operators[j]) ?
+                                           (Convert.ToDouble(op1) + Convert.ToDouble(op2)).ToString()
+                                           :
+                                           (Convert.ToDouble(op1) - Convert.ToDouble(op2)).ToString();
+
+                        if (resultOfOperation[0] != '-') resultOfOperation = resultOfOperation.Insert(0, "+");
+
+                        int op1Start = index - op1.Length;
+                        int op2End = index + op2.Length;
+
+                        // Remove the expression we just calculated, then insert in its' place the result
+                        expCopy = expCopy.Remove(op1Start, op2End - op1Start + 1).Insert(op1Start, resultOfOperation);
+
+                        hasOperatorJ = (expCopy.IndexOf(operators[j], 1) != -1);
+                        hasOperatorK = (expCopy.IndexOf(operators[j + 1], 1) != -1);
+                        indexOfOperatorJ = expCopy.IndexOf(operators[j], 1);
+                        indexOfOperatorK = expCopy.IndexOf(operators[j + 1], 1);
+                    }
+                }
+
+                display.Text = expCopy;
+
+                
+            }
+            */
+
+        private void equal_Click(object sender, EventArgs e) => display.Text = EvaluateExpression(display.Text);
+
+        public string GetOperand1(int indexOfOperator, string expression)
+        {
+            string operand1 = "";
+            int i;
+            for (i = indexOfOperator - 1; expression[i] != '+' && expression[i] != '-'; i--)
+            {
+                operand1 = operand1.Insert(0, expression[i].ToString());
+            }
+
+            operand1 = operand1.Insert(0, expression[i].ToString());
+
+            return operand1;
+        }
+
+        public string GetOperand2(int indexOfOperator, string expression)
+        {
+            string operand2 = "";
+
+            int i;
+            for (i = indexOfOperator + 1; !new[] { '+', '-', 'x', '/' }.Contains(expression[i]); i++)
+            {
+                operand2 += expression[i];
+                if (i == expression.Length - 1) break;
+            }
+
+            return operand2;
+        }
+
+        public string GetResultOfOperation(string operand1, char Operator, string operand2)
+        {
+            double operand1AsDouble = Convert.ToDouble(operand1);
+            double operand2AsDouble = Convert.ToDouble(operand2);
+            string result = "";
+            switch (Operator)
+            {
+                case '/': result = (operand1AsDouble / operand2AsDouble).ToString(); break;
+                case 'x': result = (operand1AsDouble * operand2AsDouble).ToString(); break;
+                case '+': result = (operand1AsDouble + operand2AsDouble).ToString(); break;
+                case '-': result = (operand1AsDouble - operand2AsDouble).ToString(); break;
+                default: result = ""; break;
+            }
+            return (result[0] != '-') ? result.Insert(0, "+") : result;
+        }
+
+        public string EvaluateExpression(string expression) => EvaluateAdditionAndSubtraction(EvaluateDivisionAndMultiplication(expression));
+
+        public string EvaluateDivisionAndMultiplication(string expression)
+        {
+            if (expression[0] != '+' && expression[0] != '-') expression = expression.Insert(0, "+");
+
+            string result = "";
+            bool hasDivide = (expression.IndexOf('/', 1) != -1);
+            bool hasMultiply = (expression.IndexOf('x', 1) != -1);
+
+            int indexOfDivide = expression.IndexOf('/', 1);
+            int indexOfMultiply = expression.IndexOf('x', 1);
+            while (hasDivide || hasMultiply)
+            {
+                char operatorToEvaluate;
+                int indexOfOperatorToEvaluate;
+
+                if (hasDivide && hasMultiply) indexOfOperatorToEvaluate = (indexOfDivide < indexOfMultiply) ? indexOfDivide : indexOfMultiply;
+                else indexOfOperatorToEvaluate = hasDivide ? indexOfDivide : indexOfMultiply;
+
+                operatorToEvaluate = expression[indexOfOperatorToEvaluate];
+
+                string operand1 = GetOperand1(indexOfOperatorToEvaluate, expression);
+                string operand2 = GetOperand2(indexOfOperatorToEvaluate, expression);
+                string resultOfOperation = GetResultOfOperation(operand1, operatorToEvaluate, operand2);
+
+
+                int operand1Start = indexOfOperatorToEvaluate - operand1.Length;
+                int operand2End = indexOfOperatorToEvaluate + operand2.Length;
+
+                // Remove the expression we just calculated, then insert in its' place the result
+                expression = expression.Remove(operand1Start, operand2End - operand1Start + 1).Insert(operand1Start, resultOfOperation);
+
+
+                hasDivide = (expression.IndexOf('/', 1) != -1);
+                hasMultiply = (expression.IndexOf('x', 1) != -1);
+
+                indexOfDivide = expression.IndexOf('/', 1);
+                indexOfMultiply = expression.IndexOf('x', 1);
+            }
+            result = expression;
+
+            return result;
+        }
+
+        public string EvaluateAdditionAndSubtraction(string expression)
+        {
+            if (expression[0] != '+' && expression[0] != '-') expression = expression.Insert(0, "+");
+
+            string result = "";
+            bool hasPlus = (expression.IndexOf('+', 1) != -1);
+            bool hasMinus = (expression.IndexOf('-', 1) != -1);
+
+            int indexOfPlus = expression.IndexOf('+', 1);
+            int indexOfMinus = expression.IndexOf('-', 1);
+            while (hasPlus || hasMinus)
+            {
+                char operatorToEvaluate;
+                int indexOfOperatorToEvaluate;
+
+                if (hasPlus && hasMinus) indexOfOperatorToEvaluate = (indexOfPlus < indexOfMinus) ? indexOfPlus : indexOfMinus;
+                else indexOfOperatorToEvaluate = hasPlus ? indexOfPlus : indexOfMinus;
+
+                operatorToEvaluate = expression[indexOfOperatorToEvaluate];
+
+                string operand1 = GetOperand1(indexOfOperatorToEvaluate, expression);
+                string operand2 = GetOperand2(indexOfOperatorToEvaluate, expression);
+                string resultOfOperation = GetResultOfOperation(operand1, operatorToEvaluate, operand2);
+
+
+                int operand1Start = indexOfOperatorToEvaluate - operand1.Length;
+                int operand2End = indexOfOperatorToEvaluate + operand2.Length;
+
+                // Remove the expression we just calculated, then insert in its' place the result
+                expression = expression.Remove(operand1Start, operand2End - operand1Start + 1).Insert(operand1Start, resultOfOperation);
+
+
+                hasPlus = (expression.IndexOf('+', 1) != -1);
+                hasMinus = (expression.IndexOf('-', 1) != -1);
+
+                indexOfPlus = expression.IndexOf('+', 1);
+                indexOfMinus = expression.IndexOf('-', 1);
+            }
+            result = expression;
+
+            return result;
+        }
+
+        private void DEL_Click(object sender, EventArgs e)
+        {
+            if (display.Text.Length > 0)
+                display.Text = display.Text.Remove(display.Text.Length - 1, 1);
+
+        }
+    }
+}
